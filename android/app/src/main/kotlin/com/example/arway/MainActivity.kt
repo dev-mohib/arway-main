8c84318615e89efbd8ca9bcbd4dd493b3a61cb7c
Mohib Ali <dev.mohib@gmail.com>
2022-03-05 11:06:52 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/arway/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.arway
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:arway/ar_view.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
+
+void main() {
+  runApp(ArView());
+}
